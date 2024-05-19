99cd53f8d584e8f4324a79d78f8e6a895651f9f4
Timur <Stafr95@mail.ru>
2024-05-19 19:01:16 +0500

    Adding balance

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import { useDispatch, useSelector } from "react-redux";
 
 function App() {
 	const dispatch = useDispatch();	// Это функция для изменения состояния
-	const cash = useSelector(state => state.cash);	// Получение состояния
+	const cash = useSelector(state => state.cash.cash);	// Получение состояния
 
 	const addCash = (cash) => {
 		dispatch({type: "ADD_CASH", payload: cash})	// Изменяем состояние
 	}
 
 	const getCash = (cash) => {
-		dispatch({type: "GET_CASH", payload: cash})
+		dispatch({type: "GET_CASH", payload: cash});
 	}
 
 
 	return (
 	  <div className={'app'}>
-		  <div style={{fontSize: "3rem", marginBottom: "10px"}}>{cash}</div>
+		  <div style={{fontSize: "3rem", marginBottom: "10px"}}>Баланс: {cash}</div>
 		  <div className={'buttons'}>
 			  <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
 			  <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
