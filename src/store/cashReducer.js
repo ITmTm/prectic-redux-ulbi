3449038bd8e5baa93046dab73364c2464b62e09e
Timur <Stafr95@mail.ru>
2024-05-19 18:46:50 +0500

    Separation of reducers

---
+++ src/store/cashReducer.js
@@ -0,0 +1,18 @@
+
+// Создание состояния (чаще всего это объект)
+const defaultState = {
+	cash: 5
+}
+
+
+// Создание Редьюса
+export const cashReducer = (state = defaultState, action) => {
+	switch (action.type) {
+		case "ADD_CASH":
+			return {...state, cash: state.cash + action.payload}
+		case "GET_CASH":
+			return {...state, cash: state.cash - action.payload}
+		default:
+			return state
+	}
+}
