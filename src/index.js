62c1d37ad9c6635dcfe6171ceb88245059ebd4e5
Timur <Stafr95@mail.ru>
2024-05-19 19:00:21 +0500

    Application of decomposition

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 import './style/style.scss'
 
-// Создание состояния (чаще всего это объект)
-const defaultState = {
-	cash: 5
-}
-
-// Создание Редьюса
-const reducer = (state = defaultState, action) => {
-	switch (action.type) {
-		case "ADD_CASH":
-			return {...state, cash: state.cash + action.payload}
-		case "GET_CASH":
-			return {...state, cash: state.cash - action.payload}
-		default:
-			return state
-	}
-}
-
-// Создание стора
-const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
