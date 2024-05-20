05bf4aa13aff8416b38debb40b152c21ea13ed40
Timur <Stafr95@mail.ru>
2024-05-20 17:09:49 +0500

    Creating a new action

--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -5,12 +5,15 @@ const defaultState = {
 
 
 	// Рефакторинг вынос отдельных actions в отдельные константы
-const ADD_CUSTOMER = "ADD_CUSTOMER";
+const ADD_CUSTOMER = 'ADD_CUSTOMER';
+const ADD_MANY_CUSTOMERS = 'ADD_MANY_CUSTOMERS';
 const REMOVE_CUSTOMERS = 'REMOVE_CUSTOMERS';
 
 // состояние с массивом
 export const customerReducer = (state = defaultState, action) => {
 	switch (action.type) {
+		case ADD_MANY_CUSTOMERS:
+			return {...state, customers: [...state.customers, ...action.payload]}
 		case ADD_CUSTOMER:
 			return {...state, customers: [...state.customers, action.payload]}
 		case REMOVE_CUSTOMERS:
@@ -23,4 +26,5 @@ export const customerReducer = (state = defaultState, action) => {
 
 	// Создание createActions
 export const addCustomerAction = (payload) => ({type: ADD_CUSTOMER, payload});
+export const addManyCustomersAction = (payload) => ({type: ADD_MANY_CUSTOMERS, payload});
 export const removeCustomerAction = (payload) => ({type: REMOVE_CUSTOMERS, payload});
